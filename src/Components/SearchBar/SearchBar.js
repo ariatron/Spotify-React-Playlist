abf162bc7578e04341cdeb758825989f427cdf2b
Ariana <ariana.r.lloyd@gmail.com>
2023-03-02 22:45:28 -0600

    Added a loading screen during search

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -5,14 +5,17 @@ class SearchBar extends React.Component {
     constructor(props) {
         super(props);
         
-        this.state = {term: ''};
+        this.state = {term: '', isLoading: false };
 
         this.search = this.search.bind(this);
         this.handleTermChange = this.handleTermChange.bind(this);
     }
 
     search() {
-        this.props.onSearch(this.state.term);
+        this.setState({ isLoading: true});
+        this.props.onSearch(this.state.term).then(() => {
+            this.setState({ isLoading: false });
+        });
     }
 
     handleTermChange(event) {
@@ -23,10 +26,32 @@ class SearchBar extends React.Component {
         return (
             <div className="SearchBar">
                 <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
-                <button className="SearchButton" onClick={this.search}>SEARCH</button>
+                <button className="SearchButton" onClick={this.search}>
+                    SEARCH
+                </button>
             </div>
-        )
+        );
     }
+}
+
+function LoadingScreen() {
+    return (
+        <div
+            style={{
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'rgba(0, 0, 0, 0, 0,5)',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                color: '#fff',
+                fontSize: '2rem'
+            }}
+        >Searching...</div>
+    );
 }
 
 export default SearchBar;
