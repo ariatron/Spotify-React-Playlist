72b3a8b8267df6fe5efa0c78914c2c7c67737c73
Ariana <ariana.r.lloyd@gmail.com>
2023-03-02 22:56:11 -0600

    Only include song in search results if it isn't already in the playlist

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,25 +1,20 @@
 import React from 'react';
-
-import Track from '../Track/Track';
 import './TrackList.css';
+import Track from '../Track/Track';
 
 class TrackList extends React.Component {
-renderMap() {
-    if (this.props.tracks) {
-        return this.props.tracks.map(track => {
-            return <Track track={track} key={track.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />
-    })
-        }
-    }
-    
-    render() {
-        return (
-            <div className="TrackList">
-                {this.renderMap()}
-            </div>
-        )
-    }
-}
+  render() {
+    const { tracks, playlistTracks, onAdd, onRemove, isRemoval } = this.props;
+    const filteredTracks = tracks.filter(track => !playlistTracks.some(playlistTrack => playlistTrack.id === track.id));
 
+    return (
+      <div className="TrackList">
+        {filteredTracks.map(track => {
+          return <Track key={track.id} track={track} onAdd={onAdd} onRemove={onRemove} isRemoval={isRemoval} />;
+        })}
+      </div>
+    );
+  }
+}
 
 export default TrackList;
